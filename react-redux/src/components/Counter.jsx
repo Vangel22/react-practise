60a63f7d355dd689525612ce0f69a7e4ef0a0968
Vangel22 <h.vangel22@gmail.com>
2025-02-13 13:28:25 +0100

    save class

--- react-redux/src/components/Counter.jsx
+++ react-redux/src/components/Counter.jsx
@@ -1,13 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
-import { increment } from "../state/counter/counterSlice";
+import {
+  increment,
+  decrement,
+  incrementAsync,
+} from "../state/counter/counterSlice";
 
 export const Counter = () => {
-  const dispath = useDispatch();
+  const dispatch = useDispatch();
   const count = useSelector((state) => state.counter.value);
+
   return (
     <div>
       <h1>Counter is {count}</h1>
-      <button onClick={() => dispath(increment())}>Increment</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(incrementAsync(50))}>
+        Increment async
+      </button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
     </div>
   );
 };
--- react-redux/src/state/counter/counterSlice.js
+++ react-redux/src/state/counter/counterSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const counterSlice = createSlice({
   name: "counter",
@@ -9,9 +9,33 @@ export const counterSlice = createSlice({
     increment: (state) => {
       state.value += 1;
     },
+    decrement: (state) => {
+      state.value -= 1;
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(incrementAsync.pending, (state) =>
+        console.log("Printing while pending", state)
+      )
+      .addCase(incrementAsync.fulfilled, (state, action) => {
+        console.log(state.value, action.payload);
+        state.value += action.payload;
+      })
+      .addCase(incrementAsync.rejected, (state, action) => {
+        console.error("Error: ", action.error.message);
+      });
   },
 });
 
-export const { increment } = counterSlice.actions;
+export const incrementAsync = createAsyncThunk(
+  "counter/incrementAsync",
+  async (amount) => {
+    await new Promise((res) => setTimeout(res, 3000));
+    return amount;
+  }
+);
+
+export const { increment, decrement } = counterSlice.actions;
 
 export default counterSlice.reducer;
